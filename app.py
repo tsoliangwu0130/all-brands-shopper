dc98002359fd0279fa88b1d2991f25d108a409b9
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-07-27 13:04:16 -0700

    Render var to html

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    my_name = 'Tso-Liang'
+    return render_template('index.html', my_name=my_name)
 
 
 if __name__ == '__main__':
