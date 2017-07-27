b7cc804bbf21758246fdf410affcac065d85d42c
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-07-27 13:59:41 -0700

    Render result from requested URL

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template, request
+import requests
+
+AF_URL = 'https://www.abercrombie.com/webapp/wcs/stores/servlet/Search?storeId=10051&catalogId=10901&langId=-1&departmentCategoryId=10000&search-field='
+AT_URL = 'https://www.anntaylor.com/search/searchResults.jsp?question='
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
     if request.method == 'POST':
         query = request.form['query']
-        return render_template('index.html', query=query)
+        res = requests.get(AF_URL + query)
+        return render_template('index.html', res=res.text)
     return render_template('index.html')
 
 
